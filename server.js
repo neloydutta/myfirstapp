80ef498c463ec477c6b92f6b838170467735b63c
Neloy Dutta <neloy.dutta@live.com>
2019-04-20 11:52:54 +0530

    adding procfile

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = 80
+const PORT = process.env.PORT || 3000
 
 app.get('/', (req, res) => res.send('<h1>Hello From, Ironman!</h1>'))
 
